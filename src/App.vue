b633aaeab00cc80d38b27b61eca2a22c340eb1ad
Damaris <Damaris.crews@gmail.com>
2024-11-21 08:27:36 -0500

    splash screen update

--- src/App.vue
+++ src/App.vue
@@ -31,12 +31,16 @@
                 work is needed to address this impact in the Mangrove Explorer. For now, the maps
                 provide the user with a glimpse into the future where mangroves will likely expand
                 across the majority of the upper Gulf of Mexico and up the east coast into South
-                Carolina by the Year 2100. To view the data behind this project please visit:
+                Carolina by the Year 2100. You can also view the companion
+                <a href="https://arcg.is/1jT0Sv0" target="”_blank”">story map</a> for more maps and
+                information about mangrove expansion or explore the
                 <a
-                  target="_blank"
                   href="https://www.sciencebase.gov/catalog/item/6515c966d34e469cabfcdc88"
-                  >Sciencebase.gov</a
-                ><br /><br />
+                  target="”_blank”"
+                >
+                  research</a
+                >
+                that provided the foundation for this tool. <br /><br />
                 For more information about The Nature Conservancy and partner efforts in the region
                 please visit:
                 <a
@@ -140,12 +144,16 @@
               address this impact in the Mangrove Explorer. For now, the maps provide the user with
               a glimpse into the future where mangroves will likely expand across the majority of
               the upper Gulf of Mexico and up the east coast into South Carolina by the Year 2100.
-              To view the data behind this project please visit:
+              You can also view the companion
+              <a href="https://arcg.is/1jT0Sv0" target="”_blank”">story map</a> for more maps and
+              information about mangrove expansion or explore the
               <a
-                target="_blank"
                 href="https://www.sciencebase.gov/catalog/item/6515c966d34e469cabfcdc88"
-                >Sciencebase.gov</a
-              ><br /><br />
+                target="”_blank”"
+              >
+                research</a
+              >
+              that provided the foundation for this tool.<br /><br />
               For more information about The Nature Conservancy and partner efforts in the region
               please visit:
               <a
@@ -217,55 +225,6 @@
           />
         </template>
         <template v-slot:after>
-          <!--MAP COMPONENT-->
-          <!-- <div id="splash-screen" v-if="splashDiv == true">
-            <div>
-              <q-img src="bg_image.jpg" style="height: 100vh; width: 100%; overflow-y: scroll">
-                <div class="absolute-full text-subtitle2">
-                  <div class="text-h3 text-center" style="margin: 15px">
-                    About Mangrove Explorer
-                  </div>
-                  <div
-                    style="font-weight: 300; font-size: x-large; padding: 25px; line-height: normal"
-                  >
-                    Welcome to the Mangrove Explorer Mapping Tool. The Mangrove Explorer was created
-                    by a multidisciplinary team of scientists and researchers to share cutting edge
-                    research related to the future distribution of mangroves in the continental
-                    United States. The northern limit of mangroves in this region is typically
-                    limited by the frequency and intensity of freeze events that damage or kill
-                    mangroves. However, as climate change reduces the number of freeze events,
-                    mangroves are establishing in areas north of their historic range. <br /><br />
-                    The Mangrove Explorer depicts the most current (2021) distribution of mangroves
-                    as well as the projections of the future distribution and characteristics of
-                    mangroves under future temperature and precipitation conditions. Other factors
-                    that will influence the future distribution, such as policy and coastal
-                    management, are also explored on the tool. It is extremely important to note
-                    that these projections only depict where mangroves could expand with warming
-                    temperatures and do NOT consider the devastating impacts of sea level rise on
-                    all coastal wetlands (including mangroves). Future work is needed to address
-                    this impact in the Mangrove Explorer. For now, the maps provide the user with a
-                    glimpse into the future where mangroves will likely expand across the majority
-                    of the upper Gulf of Mexico and up the east coast into the Carolinas by the Year
-                    2100.
-                  </div>
-                  <div style="margin: 30px">
-                    <q-btn
-                      @click="this.showMap()"
-                      color="primary"
-                      style="margin: 15px auto 15px auto; display: block"
-                      size="18px"
-                      >Enter</q-btn
-                    >
-                  </div>
-                  <div class="row justify-around items-end">
-                    <q-img src="UofA_Stokes.jpg" style="width: 250px" fit="contain" />
-                    <q-img src="NE_University.jpg" style="width: 250px" fit="contain" />
-                    <q-img src="TNCLogoPrimary_RGB.jpg" style="width: 250px" fit="contain" />
-                  </div>
-                </div>
-              </q-img>
-            </div>
-          </div> -->
           <the-map></the-map>
         </template>
       </q-splitter>
